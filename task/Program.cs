3efbb4fbb324137ee85529199574dd1b31ab93f7
Alex <abreskolenko@yandex.ru>
2023-03-24 20:18:26 +0300

    Task complited

--- task/Program.cs
+++ task/Program.cs
@@ -7,32 +7,35 @@ int EnterMessage(string str)
     return number;
 }
 
-var CreateArray = (int num) => new int[num];
+var CreateArray = (int sizeArray) => new string[sizeArray];
 
 void PrintArray(string[] arr)
 {
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"{arr[i]} ");
     }
 }
 
-void ResultingArray(string[] arr_1, string[] arr_2)
+string[] ResultingArray(string[] arr_1)
 {
-    for(int i = 0; i < arr_1.Length; i++)
+    string[] arr_2 = new string[arr_1.Length];
+
+    for (int i = 0; i < arr_1.Length; i++)
     {
-        if(arr_1[i] <= 3)
+        if (arr_1[i].Length <= 3)
         {
-            arr_2[i] = arr_1[i]; 
+            arr_2[i] = arr_1[i];
         }
     }
+    return arr_2;
 }
 
 void FillArray(string[] arr)
 {
     string str = String.Empty;
-
-    for(int i = 0; i < arr.Length; i++)
+    Console.WriteLine($"Заполните массив из {arr.Length} элементов : ");
+    for (int i = 0; i < arr.Length; i++)
     {
         str = Console.ReadLine();
         arr[i] = str;
@@ -43,6 +46,14 @@ void FillArray(string[] arr)
 
 int sizeArray = EnterMessage("Какой размер первоначального массива вы хотите создать ? : ");
 string[] arr_1 = CreateArray(sizeArray);
-string[] arr_2 = {};
+FillArray(arr_1);
 
+Console.WriteLine();
+
+PrintArray(arr_1);
+Console.WriteLine();
+
+Console.WriteLine("==========================================================");
+string[] arr_2 = ResultingArray(arr_1);
+PrintArray(arr_2);
 
