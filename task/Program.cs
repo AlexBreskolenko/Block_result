2b486b4246bf7fbda70a5e1b2601a9feab20f824
Alex <abreskolenko@yandex.ru>
2023-03-24 19:44:33 +0300

    Created a merhod ResultArray

--- task/Program.cs
+++ task/Program.cs
@@ -17,3 +17,19 @@ void PrintArray(int[] arr)
     }
 }
 
+void ResultingArray(int[] arr_1, int[] arr_2)
+{
+    for(int i = 0; i < arr_1.Length; i++)
+    {
+        if(arr_1[i] <= 3)
+        {
+            arr_2[i] = arr_1[i]; 
+        }
+    }
+}
+
+int sizeArray = EnterMessage("Какой размер первоначального массива вы хотите создать ? : ");
+int[] arr_1 = CreateArray(sizeArray);
+int[] arr_2 = {};
+
+
