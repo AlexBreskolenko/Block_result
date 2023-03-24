881668a32bdc997f50592c9c7e50178fdb260f58
Alex <abreskolenko@yandex.ru>
2023-03-24 19:30:07 +0300

    Created a method PrintArray

--- task/Program.cs
+++ task/Program.cs
@@ -9,3 +9,11 @@ int EnterMessage(string str)
 
 var CreateArray = (int num) => new int[num];
 
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
